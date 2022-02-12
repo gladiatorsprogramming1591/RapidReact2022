f7ad1b773b0b4bde6ba1d70eb6bd9edb0a009ca9
Grace <purrlgirl215@gmail.com>
2022-02-12 11:44:48 -0500

    More Intake Stuff

--- RapidReact2022/src/main/java/frc/robot/Constants.java
+++ RapidReact2022/src/main/java/frc/robot/Constants.java
@@ -1,5 +1,7 @@
 package frc.robot;
 
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+
 public final class Constants {
     // Controller Ports
     public final static int kDriverControllerPort = 0;
@@ -13,4 +15,11 @@ public final class Constants {
     public final static double kShooterGearRatio = 7/6;
     public final static double kNeoMaxSpeed = 5676;
     public final static double kShooterMotorSpeedRPM = kShooterMotorSpeed*kNeoMaxSpeed*kShooterGearRatio;
+    public static final PneumaticsModuleType kPCM_CANID = null;
+    public static final int kArmSolenoidForwardChannel = 0; //tbd
+    public static final int kArmSolenoidReverseChannel = 1; //tbd
+    public static final double kIntakeForwardSpeed = .5;
+    public static final double kIntakeReverseSpeed = -.5;
+    public static final double kIntakeRampRate = 0.1;
+    public static final int kIntakeChannel = 5; //tbd
 }
--- RapidReact2022/src/main/java/frc/robot/RobotContainer.java
+++ RapidReact2022/src/main/java/frc/robot/RobotContainer.java
@@ -4,21 +4,27 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-import edu.wpi.first.wpilibj2.command.button.POVButton;
+import edu.wpi.first.wpilibj2.command.button.POVButton; //use later
 import frc.robot.commands.*;
+import frc.robot.commands.IntakeCommands.IntakeOn;
 import frc.robot.commands.ShooterCommands.ShooterOff;
 import frc.robot.commands.ShooterCommands.ShooterOn;
+import frc.robot.subsystems.IntakeSubsystem;
 import frc.robot.subsystems.ShooterSubsystem;
 
 public class RobotContainer {
     private final ShooterSubsystem m_shooterSubsystem = new ShooterSubsystem();
+    private final IntakeSubsystem m_IntakeSubsystem = new IntakeSubsystem();
     public final static Joystick m_driverStick = new Joystick(Constants.kDriverControllerPort);
+
 
     RobotContainer() { 
             System.out.println("RobotContainer c'tor");
         SmartDashboard.putString("Hello World", "I am Robot");
         new JoystickButton(m_driverStick, JoystickButtonConstants.kX)
         .whileHeld(new XButtonTest()); 
+        new JoystickButton(m_driverStick, JoystickButtonConstants.kX)
+        .whenPressed((Command) new IntakeOn(m_IntakeSubsystem)); 
         // new JoystickButton(m_driverStick, JoystickButtonConstants.kB)
         // .whenPressed(new MotorTest(m_driverStick)); 
 
