0bc9952e566fb506de34fc09050bf5f38602721d
Jeff Tuttle <jefft138@gmail.com>
2022-03-10 16:20:56 -0500

    Add WallAuto command
    Shoot low goal, back up,

--- RapidReact2022/src/main/java/frc/robot/Robot.java
+++ RapidReact2022/src/main/java/frc/robot/Robot.java
@@ -9,7 +9,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import frc.robot.commands.AutoCommands.PickupShootLowGoal;
+import frc.robot.commands.AutoCommands.WallAuto;
 import frc.robot.subsystems.DriveTrain;
 import frc.robot.subsystems.DriveTrainC;
 
